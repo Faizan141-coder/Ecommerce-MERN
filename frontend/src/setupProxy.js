bc4170ac776ca9f91730de703fb47b8a4f010088
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2023-06-19 16:33:24 +0500

    Update setupProxy.js

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
     app.use(
       ["/user/","/product/","/order/","/delivery/","/checkout/"],
       createProxyMiddleware({
-        target: "http://localhost:5000",
+        target: "https://ecommerce-mern-backend-nine.vercel.app/",
         changeOrigin: true,
       })
     );
